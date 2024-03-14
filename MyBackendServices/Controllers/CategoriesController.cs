f3145ef7f3d5b7a82bf493318d93ef1f52028910
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-03-14 13:43:16 +0700

    menambahkan method GetByName

--- MyBackendServices/Controllers/CategoriesController.cs
+++ MyBackendServices/Controllers/CategoriesController.cs
@@ -8,14 +8,14 @@ namespace MyBackendServices.Controllers
     public class CategoriesController : ControllerBase
     {
         private static List<Category> categories = new List<Category>
-            {
+        {
                 new Category { CategoryID = 1, CategoryName = "Beverages" },
                 new Category { CategoryID = 2, CategoryName = "Condiments" },
                 new Category { CategoryID = 3, CategoryName = "Confections" },
                 new Category { CategoryID = 4, CategoryName = "Dairy Products" },
                 new Category { CategoryID = 5, CategoryName = "Grains/Cereals" },
                 new Category { CategoryID = 6, CategoryName = "Meat/Poultry" },
-            };
+        };
 
         public CategoriesController()
         {
@@ -29,14 +29,14 @@ namespace MyBackendServices.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById(int id)
+        public Category GetById(int id)
         {
             var category = categories.SingleOrDefault(c => c.CategoryID == id);
             if (category == null)
             {
-                return NotFound($"CategoryID {id} not found!");
+                throw new ArgumentException($"Not found {id}");
             }
-            return Ok(category);
+            return category;
         }
 
         [HttpPost]
@@ -46,5 +46,39 @@ namespace MyBackendServices.Controllers
             return CreatedAtAction(nameof(GetById),
                 new { id = category.CategoryID }, category);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Category category)
+        {
+            var result = GetById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            result.CategoryName = category.CategoryName;
+            return Ok($"Data Category {id} berhasil diupdate !");
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = GetById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            categories.Remove(result);
+            return Ok($"Data Category ID: {id} berhasil didelete");
+        }
+
+        //api/v1/Categories/GetByName?name=Con
+        [HttpGet("GetByName")]
+        public IEnumerable<Category> GetByName(string name)
+        {
+            var results = categories.Where(c => c.CategoryName.Contains(name));
+            return results;
+        }
+
     }
 }
