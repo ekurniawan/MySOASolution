7f320fc6aa8c7e0bef54be372274a8bbc19a2e17
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-03-27 13:31:40 +0700

    change connection string

--- MyBackendServices/Program.cs
+++ MyBackendServices/Program.cs
@@ -76,11 +76,11 @@ builder.Services.AddDbContext<SamuraiContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
